d3e9bc89ac022924fe9f68e8947a94cce35a1c23
Baratoo <kaique_luiz1@hotmail.com>
2024-10-09 09:50:22 -0300

    layout principal

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponent
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor() {
+
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+   }
 
   ngOnInit(): void {
   }
