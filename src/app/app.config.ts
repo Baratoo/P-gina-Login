2153640cd0511c665fda191cecd7ee12c500fd60
Baratoo <kaique_luiz1@hotmail.com>
2024-10-10 10:32:00 -0300

    att

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,11 @@ import { provideRouter } from "@angular/router";
 import { ApplicationConfig } from "@angular/platform-browser";
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient(withFetch())
+        
     ]
 }
